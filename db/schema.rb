4c4257a8f2c2e325c89284ad0a38453cfb825285
Ryan Lo <lowc1012@gmail.com>
2023-01-29 16:28:04 +0800

    add a new column(views) to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_111154) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_132025) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
